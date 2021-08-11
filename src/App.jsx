45d8311e8d1534aa1c40471fd6f0d89c39d84a13
四方 秀 <shu.42111227@gmail.com>
2021-08-12 00:42:03 +0900

    再レンダリングのバグを修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("さいしょ");
+
   const [num, setNum] = useState(0);
+  const [showFlag, setShowFlag] = useState(false);
 
   const onClickCountUP = () => {
     setNum(num + 1);
   };
+
+  const onClickShowFlag = () => {
+    setShowFlag(!showFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        showFlag || setShowFlag(true);
+      } else {
+        showFlag && setShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line
+  }, [num]);
 
   return (
     <>
@@ -14,7 +32,10 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
       <button onClick={onClickCountUP}>カウントアップ</button>
+      <br />
+      <button onClick={onClickShowFlag}>on/off</button>
       <p>{num}</p>
+      {showFlag && <p>( ͡° ͜ʖ ͡°)</p>}
     </>
   );
 };
